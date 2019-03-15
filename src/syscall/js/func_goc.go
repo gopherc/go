c35fed86e76993a6d32ff68fdfcec3125f81f5d9
Andreas T Jonsson <mail@andreasjonsson.se>
2019-03-15 08:33:36 +0100

    Added stubs.

---
+++ src/syscall/js/func_goc.go
@@ -0,0 +1,17 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build js,wasm,goc
+
+package js
+
+// setEventHandler is defined in the runtime package.
+func setEventHandler(fn func())
+
+func init() {
+	setEventHandler(handleEvent)
+}
+
+func handleEvent() {
+}
